97e07f092ca4f6ab21fe5f9db011b4c25c203e9c
Arka Nath <nath2107055@stud.kuet.ac.bd>
2024-07-15 03:01:44 +0600

    first commit

---
+++ my-project/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="my-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-cyan-400 text-xl font-medium text-indigo-950">
+      <img src="/logos_firebase.png" />
+      <h1>Contact Web App</h1>
+    </div>
+  );
+};
+
+export default Navbar;
